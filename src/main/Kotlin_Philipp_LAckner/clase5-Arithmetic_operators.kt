31d38384409c802525127ed5d5f6db6572a9d135
Cristian Lotero Aquino <cristianlotero85@gmail.com>
2025-06-29 23:08:04 -0300

    actualizando practicas de Kotlin_Philipp_Lackner

--- src/main/Kotlin_Philipp_LAckner/clase5-Arithmetic_operators.kt
+++
@@ -1,8 +0,0 @@
-
-
-fun main() {
-    val x = 12
-    val y = 6
-    val result = x * y
-    print("El resultado de la operacion es: $result")
-}
---
+++ src/main/kotlin/Kotlin_Philipp_LAckner/clase5-Arithmetic_operators.kt
@@ -0,0 +1,12 @@
+fun main() {
+    var x = 12
+    var y = 6
+    val result = x * y
+    print("El resultado de la operacion es: $result \n")
+//    y = y + 3
+    y += 3
+    println("The result of X + Y is ${x * y}")
+
+
+
+}
