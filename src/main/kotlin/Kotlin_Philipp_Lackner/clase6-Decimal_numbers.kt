3190c631d311fec2e218f717421b883fb104c161
Cristian Lotero Aquino <cristianlotero85@gmail.com>
2025-06-30 22:08:14 -0300

    creacion de archivo de practica clase6

---
+++ src/main/kotlin/Kotlin_Philipp_Lackner/clase6-Decimal_numbers.kt
@@ -0,0 +1,10 @@
+package Kotlin_Philipp_Lackner
+
+fun main(){
+
+
+    var x  = 20.3546798356456457334563456
+    var y  = 5.2
+    var result = x / y
+    println("The result of x / y is: ${result}")
+}
