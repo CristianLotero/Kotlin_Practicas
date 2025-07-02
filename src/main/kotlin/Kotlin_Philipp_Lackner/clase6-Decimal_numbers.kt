10fe0e8080529630dd713a72426abb6bc1a6d259
Cristian Lotero Aquino <cristianlotero85@gmail.com>
2025-07-01 22:44:18 -0300

    practica kotlin clase 7 - strings

--- src/main/kotlin/Kotlin_Philipp_Lackner/clase6-Decimal_numbers.kt
+++ src/main/kotlin/Kotlin_Philipp_Lackner/clase6-Decimal_numbers.kt
@@ -1,10 +1,26 @@
 package Kotlin_Philipp_Lackner
 
-fun main(){
+import org.example.PI
+import kotlin.math.pow
 
+//fun main(){
+//
+//
+//    var x  = 20.3546798356456457334563456
+//    var y  = 5.2
+//    var result = x / y
+//    println("The result of x / y is: ${result}")
+//}
 
-    var x  = 20.3546798356456457334563456
-    var y  = 5.2
-    var result = x / y
-    println("The result of x / y is: ${result}")
+//Use this formula for the volume: 0.75 * pi * radius^3
+// The volume of the sphere with the radius 5.5 is 392.0118578578125
+
+fun main() {
+    var height: Double  = 0.75
+    var pi = kotlin.math.PI
+    var radius: Double = 5.5
+    var exponent: Double = 3.0
+
+//    println(pi)
+    println("The volume of the sphere is: ${height * pi * radius.pow(3)}")
 }
---
+++ src/main/kotlin/Kotlin_Philipp_Lackner/clase7-strings.kt
@@ -0,0 +1,10 @@
+package Kotlin_Philipp_Lackner
+
+fun main() {
+    var myString: String   = "This is an example string"
+    println("I am practicing new lines \n in the same println()")
+
+    println("I'm looking realy good stuff here: ${myString.uppercase()}")
+
+
+}
