79831ad71fb1bc3ab39956a750199878c521512e
Cristian Lotero Aquino <cristianlotero85@gmail.com>
2025-07-02 22:54:43 -0300

    Actualizado practica operadores logicos y de comparacion

--- src/main/kotlin/Kotlin_Philipp_Lackner/clase7-strings.kt
+++ src/main/kotlin/Kotlin_Philipp_Lackner/clase7-strings.kt
@@ -2,9 +2,10 @@ package Kotlin_Philipp_Lackner
 
 fun main() {
     var myString: String   = "This is an example string"
-    println("I am practicing new lines \n in the same println()")
+    var myName = "Cristian Lotero Aquino"
 
-    println("I'm looking realy good stuff here: ${myString.uppercase()}")
+//    println("I am practicing new lines \n in the same println()")
+//    println("I'm looking at realy good stuff here: ${myString.uppercase()")
 
-
+    println("God whispered this to my parents ears: ${myName.uppercase()} \n and then the enthropy do this: ${myName.uppercase().reversed()}")
 }
---
+++ src/main/kotlin/Kotlin_Philipp_Lackner/clase8-logical_and_comparison_operators.kt
@@ -0,0 +1,15 @@
+package Kotlin_Philipp_Lackner
+
+fun main() {
+    val x = 5.0
+    val y = 5.0
+    val a = 30
+    val b = 180
+    val equality = x == y
+    val lessOrEqualThan = x <= y
+    val differentThan = x != y
+    val and = x == y  && a == b
+    val or = x == y  || a == b
+
+    println("the value is: ${}")
+}
